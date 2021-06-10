033af20c0a27087f4b29b3e8418296178044d8a1
rknaik76 <rknaik76@gmail.com>
2021-06-10 15:43:07 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,11 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
+  backend "azure" {}
 }
 
 # Configure the Microsoft Azure Provider
-
+provider "azurerm" {
+  features {}
+}
 
 resource "azurerm_resource_group" "samplerg01" {
   name = "samplerg02"

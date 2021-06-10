f94b402740a22c02a7190608ba3c6da6c7413343
rknaik76 <rknaik76@gmail.com>
2021-06-10 14:35:44 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
 
+# Configure the Microsoft Azure Provider
+
+
+resource "azurerm_resource_group" "samplerg01" {
+  name = "samplerg02"
+  location = "eastus"
+
+  tags = {
+    "rgowner" = "ranjit"
+  }
+}

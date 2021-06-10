1ac8af43658e626242a24274494917e8259d41ac
rknaik76 <rknaik76@gmail.com>
2021-06-10 14:36:46 +0100

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+    subscription_id = "${var.subscription_id}"
+    client_id = "${var.client_id}"
+    client_secret = "${var.client_secret}"
+    tenant_id = "${var.tenant_id}"
+  features {}
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
